6aaa09508c41e41df81ed98f575fadaf11452037
sguest <spencer.b.guest@gmail.com>
2024-05-01 21:21:06 -0500

    Rename to peaceful tech tweaks, various updates

--- src/data.ts
+++ src/data.ts
@@ -1,81 +1,56 @@
 /// <reference path="../node_modules/factorio-types/index.d.ts" />
 
-export {};
-
-data.raw.technology['power-armor-mk2'].unit.ingredients = [
-    ['automation-science-pack', 1],
-    ['logistic-science-pack', 1],
-    ['chemical-science-pack', 1],
-    ['utility-science-pack', 1],
-];
-
-data.raw.technology['power-armor-mk2'].prerequisites = [
-    'power-armor',
-    'effectivity-module-2',
-    'speed-module-2',
-];
-
-data.raw.technology['fusion-reactor-equipment'].unit.ingredients = [
-    ['automation-science-pack', 1],
-    ['logistic-science-pack', 1],
-    ['chemical-science-pack', 1],
-    ['utility-science-pack', 1],
-];
-
-data.raw.technology['fusion-reactor-equipment'].prerequisites = [
-    'utility-science-pack',
-    'power-armor',
-];
-
-if(data.raw.technology['bob-power-armor-3']) {
-    data.raw.technology['bob-power-armor-3'].unit.ingredients = [
-        ['automation-science-pack', 1],
-        ['logistic-science-pack', 1],
-        ['chemical-science-pack', 1],
-        ['utility-science-pack', 1],
-    ];
-
-    data.raw.technology['bob-power-armor-4'].unit.ingredients = [
-        ['automation-science-pack', 1],
-        ['logistic-science-pack', 1],
-        ['chemical-science-pack', 1],
-        ['utility-science-pack', 1],
-    ];
-
-    data.raw.technology['bob-power-armor-5'].unit.ingredients = [
-        ['automation-science-pack', 1],
-        ['logistic-science-pack', 1],
-        ['chemical-science-pack', 1],
-        ['utility-science-pack', 1],
-    ];
+function removePrerequisite(techName: string, prerequisite: string) {
+    let technology = data.raw.technology[techName];
+    if(technology) {
+        for(const [index, name] of pairs(technology.prerequisites)) {
+            if(name === prerequisite) {
+                table.remove(technology.prerequisites, index as number);
+            }
+        }
+    }
 }
 
-// This is also bob's warfare and could probably share the power armor check, but this feels cleaner
-if(data.raw.technology['radars-3']) {
-    data.raw.technology['radars-3'].prerequisites = [
-        'radars-2',
-        'advanced-electronics',
-    ];
+type sciencePackIngredient = [string, number];
 
-    data.raw.technology['radars-5'].prerequisites = [
-        'radars-4',
-    ];
+function removeMilitaryScience(techName: string) {
+    let technology = data.raw.technology[techName];
+
+    if(technology) {
+        for(const [index, ingredient] of pairs(technology.unit.ingredients)) {
+            if((ingredient as sciencePackIngredient)[0] === 'military-science-pack')  {
+                table.remove(technology.unit.ingredients, index as number)
+            }
+        }
+    }
 }
 
-if(data.raw.technology['fusion-reactor-equipment-3']) {
-    data.raw.technology['fusion-reactor-equipment-3'].unit.ingredients = [
-        ['automation-science-pack', 1],
-        ['logistic-science-pack', 1],
-        ['chemical-science-pack', 1],
-        ['production-science-pack', 1],
-        ['utility-science-pack', 1],
-    ];
+removeMilitaryScience('power-armor-mk2');
+removePrerequisite('power-armor-mk2', 'military-4');
 
-    data.raw.technology['fusion-reactor-equipment-4'].unit.ingredients = [
-        ['automation-science-pack', 1],
-        ['logistic-science-pack', 1],
-        ['chemical-science-pack', 1],
-        ['production-science-pack', 1],
-        ['utility-science-pack', 1],
-    ];
+removeMilitaryScience('fusion-reactor-equipment');
+removePrerequisite('fusion-reactor-equipment', 'military-science-pack');
+
+removeMilitaryScience('spidertron');
+removePrerequisite('spidertron', 'military-4');
+
+if(mods['bobequipment']) {
+    removeMilitaryScience('fusion-reactor-equipment-3');
+    removeMilitaryScience('fusion-reactor-equipment-4');
+}
+
+if(mods['bobwarfare']) {
+    removeMilitaryScience('bob-power-armor-3');
+    removeMilitaryScience('bob-power-armor-4');
+    removeMilitaryScience('bob-power-armor-5');
+
+    removePrerequisite('radars-3', 'military-3');
+    removePrerequisite('radars-5', 'military-4');
+
+    removeMilitaryScience('walking-vehicle');
+    removePrerequisite('walking-vehicle', 'military-3');
+    removeMilitaryScience('tankotron');
+    removePrerequisite('tankotron', 'tank');
+    removeMilitaryScience('logistic-spidertron');
+    removeMilitaryScience('heavy-spidertron');
 }
